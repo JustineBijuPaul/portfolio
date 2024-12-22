b9b50ce36ed340ade350f2dce02eb2286225d7a6
Justine Biju Paul <justinebijupaul@gmail.com>
2024-12-22 14:40:32 +0530

    buttuon update

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className="cta">
+        <a href="resume.pdf" className="btn" download='atavares.pdf'>
+            Curriculo 
+        </a>
+
+        <a href="https://blog.atavares.com" className="btn btn-primary" target="_blank" rel="noreferrer">
+          BLOG
+        </a>
+    </div>
+  )
+}
+
+export default CTA
